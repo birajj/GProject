ae3533618e9e943d2311872a0e7c2d4393f8711a
birajj <james.biraj@gmail.com>
2015-09-09 20:39:00 +0545

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+global $config;
+$config = array();
+$config['site_path']	=	$_SERVER['DOCUMENT_ROOT'].'/GProject/';
+$config['site_url']		=	'/GProject/';
+$config['img_path']		=	$config['site_url']."library/images/";
+$config['css_path']		=	$config['site_url']."library/css/";
+?>
